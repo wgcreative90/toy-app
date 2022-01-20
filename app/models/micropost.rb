7fa9266bb8107b21d8810619fd2a227b4782037a
William George <wgcreative@protonmail.com>
2022-01-20 18:50:47 -0500

    First shot at email validation via REGEX_EXPRESSION. COME BACK AND PERFECT.

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,4 @@
-class Micropost < ActiveRecord::Base
+class Micropost < ApplicationRecord
   belongs_to :user
   validates :content, length: { maximum: 140 }, presence: true
   validates :user_id, length: { minimum: 1, maximum: 7 }, presence: true
