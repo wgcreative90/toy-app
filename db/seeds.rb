cfbcf1ce726b53523acaf3a875e2cc2ea4975738
William George <wgcreative@protonmail.com>
2022-01-20 16:23:53 -0500

    Rewording comments in seeds.rb to reflect code changes in previous commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
-# Seed 6 test User instances in DB.
+# Seed 100 fake User instances.
 100.times do
   first_name = FFaker::Name.first_name
   last_name = FFaker::Name.last_name
@@ -11,7 +10,7 @@
   User.create(name: full_name, email: email)
 end
 
-# Seed 6 User Micropost instances in DB assigning them to the first 6 user_id's
+# Seed 100 fake Micropost instances
 100.times do |i|
   content = FFaker::Tweet.body(140)
   Micropost.create(content: content, user_id: i + 1)
