d020fa91cc3c4f1abbdf8b55f1ae27f9eb6e068e
William George <wgcreative@protonmail.com>
2022-01-20 12:43:43 -0500

    Modify-Gitignore: dont commit code editor settings to repo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
-    def hello
-        render html: "hello, world!"
-    end
-    
+  def hello
+    render html: 'hello, world!'
+  end
+
+  def goodbye( message = 'Goodbye, ' )
+    render html: message.to_s.zero? ? 'Goodbye, world!' : 'Goodbye, ' + message
+  end
 end
