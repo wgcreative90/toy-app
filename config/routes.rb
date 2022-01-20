ca1c5e2e5733703db040fcccaa4cedf10a885412
William George <wgcreative@protonmail.com>
2022-01-20 13:12:38 -0500

    Update-ROUTES-set root to users#index action in UsersController

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :users
-  get 'users/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'users#index'
 end
