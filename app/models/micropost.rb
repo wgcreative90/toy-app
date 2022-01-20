78b5872248240bc5f3000fce6afaa02a5548b1f1
William George <wgcreative@protonmail.com>
2022-01-20 17:29:44 -0500

    Add relationships and  validations to models - Validates min and max length's, presence, and uniqueness.

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,5 @@
-class Micropost < ApplicationRecord
-  validates :content, length: { maximum: 140 }
+class Micropost < ActiveRecord::Base
+  belongs_to :user
+  validates :content, length: { maximum: 140 }, presence: true
+  validates :user_id, length: { minimum: 1, maximum: 7 }, presence: true
 end
