c6e8ecfc72388be58d947d64e309751d35a56088
William George <wgcreative@protonmail.com>
2022-01-20 15:02:09 -0500

    Modify-Gemfile: add FFaker gem and update seeds.rb to generate test User's and Miscroposts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,18 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-# User.create(name: Faker.name, email: Faker.email)
-# User.create(name: Faker.name, email: Faker.email)
-# User.create(name: Faker.name, email: Faker.email)
+
+# Seed 6 test User instances in DB.
+6.times do
+  first_name = FFaker::Name.first_name
+  last_name = FFaker::Name.last_name
+  full_name = first_name + ' ' + last_name
+  email = first_name + '.' + last_name + '@testdata.com'
+  User.create(name: full_name, email: email)
+end
+
+# Seed 6 User Micropost instances in DB assigning them to the first 6 user_id's
+6.times do |i|
+  content = FFaker::Tweet.body(140)
+  Micropost.create(content: content, user_id: i)
+end
